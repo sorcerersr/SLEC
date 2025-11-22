e3b2cc6d75dd685df0b047368ccecaa020cf20f0
Stefan Rabmund <stefan.rabmund@mailbox.org>
2025-11-22 21:39:01 +0100

    some tests

--- src/model.rs
+++ src/model.rs
@@ -168,164 +168,95 @@ mod tests {
     fn test_shutter_speed_array_values() {
         let speeds = ShutterSpeed::shutter_speed_array();
 
-        // Test all the shutter speed values and their display text
-        assert_eq!(speeds[0].display_text, "1/8000");
-        assert_eq!(speeds[0].speed_value, 1.0 / 8000.0);
+        // Define expected values for verification
+        let expected_values = [
+            ("1/8000", 1.0 / 8000.0),
+            ("1/6400", 1.0 / 6400.0),
+            ("1/5000", 1.0 / 5000.0),
+            ("1/4000", 1.0 / 4000.0),
+            ("1/3200", 1.0 / 3200.0),
+            ("1/2500", 1.0 / 2500.0),
+            ("1/2000", 1.0 / 2000.0),
+            ("1/1600", 1.0 / 1600.0),
+            ("1/1250", 1.0 / 1250.0),
+            ("1/800", 1.0 / 800.0),
+            ("1/640", 1.0 / 640.0),
+            ("1/500", 1.0 / 500.0),
+            ("1/400", 1.0 / 400.0),
+            ("1/320", 1.0 / 320.0),
+            ("1/250", 1.0 / 250.0),
+            ("1/200", 1.0 / 200.0),
+            ("1/160", 1.0 / 160.0),
+            ("1/125", 1.0 / 125.0),
+            ("1/100", 1.0 / 100.0),
+            ("1/80", 1.0 / 80.0),
+            ("1/60", 1.0 / 60.0),
+            ("1/50", 1.0 / 50.0),
+            ("1/40", 1.0 / 40.0),
+            ("1/30", 1.0 / 30.0),
+            ("1/25", 1.0 / 25.0),
+            ("1/20", 1.0 / 20.0),
+            ("1/15", 1.0 / 15.0),
+            ("1/10", 1.0 / 10.0),
+            ("1/8", 1.0 / 8.0),
+            ("1/6", 1.0 / 6.0),
+            ("1/5", 1.0 / 5.0),
+            ("1/4", 1.0 / 4.0),
+            ("0.3", 0.3),
+            ("0.4", 0.4),
+            ("0.5", 0.5),
+            ("0.6", 0.6),
+            ("0.8", 0.8),
+            ("1", 1.0),
+            ("1.3", 1.3),
+            ("1.6", 1.6),
+            ("2", 2.0),
+            ("2.5", 2.5),
+            ("3.2", 3.2),
+            ("4", 4.0),
+            ("5", 5.0),
+            ("6", 6.0),
+            ("8", 8.0),
+            ("10", 10.0),
+            ("13", 13.0),
+            ("15", 15.0),
+            ("20", 20.0),
+            ("25", 25.0),
+            ("30", 30.0),
+        ];
 
-        assert_eq!(speeds[1].display_text, "1/6400");
-        assert_eq!(speeds[1].speed_value, 1.0 / 6400.0);
+        // Verify each element
+        for (i, (expected_text, expected_value)) in expected_values.iter().enumerate() {
+            assert_eq!(speeds[i].display_text, *expected_text);
+            assert_eq!(speeds[i].speed_value, *expected_value);
+        }
+    }
 
-        assert_eq!(speeds[2].display_text, "1/5000");
-        assert_eq!(speeds[2].speed_value, 1.0 / 5000.0);
+    #[test]
+    fn test_filter_default_list() {
+        let filters = Filter::default_filter_list();
 
-        assert_eq!(speeds[3].display_text, "1/4000");
-        assert_eq!(speeds[3].speed_value, 1.0 / 4000.0);
+        assert_eq!(filters.len(), 3);
 
-        assert_eq!(speeds[4].display_text, "1/3200");
-        assert_eq!(speeds[4].speed_value, 1.0 / 3200.0);
+        // Check first filter
+        assert_eq!(filters[0].factor, 8);
+        assert_eq!(filters[0].fstop_reduction, 3.0);
+        assert_eq!(filters[0].display_name, "ND8");
+        assert_eq!(filters[0].selected, false);
+        assert_eq!(filters[0].id, 0);
 
-        assert_eq!(speeds[5].display_text, "1/2500");
-        assert_eq!(speeds[5].speed_value, 1.0 / 2500.0);
+        // Check second filter
+        assert_eq!(filters[1].factor, 64);
+        assert_eq!(filters[1].fstop_reduction, 6.0);
+        assert_eq!(filters[1].display_name, "ND64");
+        assert_eq!(filters[1].selected, false);
+        assert_eq!(filters[1].id, 1);
 
-        assert_eq!(speeds[6].display_text, "1/2000");
-        assert_eq!(speeds[6].speed_value, 1.0 / 2000.0);
-
-        assert_eq!(speeds[7].display_text, "1/1600");
-        assert_eq!(speeds[7].speed_value, 1.0 / 1600.0);
-
-        assert_eq!(speeds[8].display_text, "1/1250");
-        assert_eq!(speeds[8].speed_value, 1.0 / 1250.0);
-
-        assert_eq!(speeds[9].display_text, "1/800");
-        assert_eq!(speeds[9].speed_value, 1.0 / 800.0);
-
-        assert_eq!(speeds[10].display_text, "1/640");
-        assert_eq!(speeds[10].speed_value, 1.0 / 640.0);
-
-        assert_eq!(speeds[11].display_text, "1/500");
-        assert_eq!(speeds[11].speed_value, 1.0 / 500.0);
-
-        assert_eq!(speeds[12].display_text, "1/400");
-        assert_eq!(speeds[12].speed_value, 1.0 / 400.0);
-
-        assert_eq!(speeds[13].display_text, "1/320");
-        assert_eq!(speeds[13].speed_value, 1.0 / 320.0);
-
-        assert_eq!(speeds[14].display_text, "1/250");
-        assert_eq!(speeds[14].speed_value, 1.0 / 250.0);
-
-        assert_eq!(speeds[15].display_text, "1/200");
-        assert_eq!(speeds[15].speed_value, 1.0 / 200.0);
-
-        assert_eq!(speeds[16].display_text, "1/160");
-        assert_eq!(speeds[16].speed_value, 1.0 / 160.0);
-
-        assert_eq!(speeds[17].display_text, "1/125");
-        assert_eq!(speeds[17].speed_value, 1.0 / 125.0);
-
-        assert_eq!(speeds[18].display_text, "1/100");
-        assert_eq!(speeds[18].speed_value, 1.0 / 100.0);
-
-        assert_eq!(speeds[19].display_text, "1/80");
-        assert_eq!(speeds[19].speed_value, 1.0 / 80.0);
-
-        assert_eq!(speeds[20].display_text, "1/60");
-        assert_eq!(speeds[20].speed_value, 1.0 / 60.0);
-
-        assert_eq!(speeds[21].display_text, "1/50");
-        assert_eq!(speeds[21].speed_value, 1.0 / 50.0);
-
-        assert_eq!(speeds[22].display_text, "1/40");
-        assert_eq!(speeds[22].speed_value, 1.0 / 40.0);
-
-        assert_eq!(speeds[23].display_text, "1/30");
-        assert_eq!(speeds[23].speed_value, 1.0 / 30.0);
-
-        assert_eq!(speeds[24].display_text, "1/25");
-        assert_eq!(speeds[24].speed_value, 1.0 / 25.0);
-
-        assert_eq!(speeds[25].display_text, "1/20");
-        assert_eq!(speeds[25].speed_value, 1.0 / 20.0);
-
-        assert_eq!(speeds[26].display_text, "1/15");
-        assert_eq!(speeds[26].speed_value, 1.0 / 15.0);
-
-        assert_eq!(speeds[27].display_text, "1/10");
-        assert_eq!(speeds[27].speed_value, 1.0 / 10.0);
-
-        assert_eq!(speeds[28].display_text, "1/8");
-        assert_eq!(speeds[28].speed_value, 1.0 / 8.0);
-
-        assert_eq!(speeds[29].display_text, "1/6");
-        assert_eq!(speeds[29].speed_value, 1.0 / 6.0);
-
-        assert_eq!(speeds[30].display_text, "1/5");
-        assert_eq!(speeds[30].speed_value, 1.0 / 5.0);
-
-        assert_eq!(speeds[31].display_text, "1/4");
-        assert_eq!(speeds[31].speed_value, 1.0 / 4.0);
-
-        assert_eq!(speeds[32].display_text, "0.3");
-        assert_eq!(speeds[32].speed_value, 0.3);
-
-        assert_eq!(speeds[33].display_text, "0.4");
-        assert_eq!(speeds[33].speed_value, 0.4);
-
-        assert_eq!(speeds[34].display_text, "0.5");
-        assert_eq!(speeds[34].speed_value, 0.5);
-
-        assert_eq!(speeds[35].display_text, "0.6");
-        assert_eq!(speeds[35].speed_value, 0.6);
-
-        assert_eq!(speeds[36].display_text, "0.8");
-        assert_eq!(speeds[36].speed_value, 0.8);
-
-        assert_eq!(speeds[37].display_text, "1");
-        assert_eq!(speeds[37].speed_value, 1.0);
-
-        assert_eq!(speeds[38].display_text, "1.3");
-        assert_eq!(speeds[38].speed_value, 1.3);
-
-        assert_eq!(speeds[39].display_text, "1.6");
-        assert_eq!(speeds[39].speed_value, 1.6);
-
-        assert_eq!(speeds[40].display_text, "2");
-        assert_eq!(speeds[40].speed_value, 2.0);
-
-        assert_eq!(speeds[41].display_text, "2.5");
-        assert_eq!(speeds[41].speed_value, 2.5);
-
-        assert_eq!(speeds[42].display_text, "3.2");
-        assert_eq!(speeds[42].speed_value, 3.2);
-
-        assert_eq!(speeds[43].display_text, "4");
-        assert_eq!(speeds[43].speed_value, 4.0);
-
-        assert_eq!(speeds[44].display_text, "5");
-        assert_eq!(speeds[44].speed_value, 5.0);
-
-        assert_eq!(speeds[45].display_text, "6");
-        assert_eq!(speeds[45].speed_value, 6.0);
-
-        assert_eq!(speeds[46].display_text, "8");
-        assert_eq!(speeds[46].speed_value, 8.0);
-
-        assert_eq!(speeds[47].display_text, "10");
-        assert_eq!(speeds[47].speed_value, 10.0);
-
-        assert_eq!(speeds[48].display_text, "13");
-        assert_eq!(speeds[48].speed_value, 13.0);
-
-        assert_eq!(speeds[49].display_text, "15");
-        assert_eq!(speeds[49].speed_value, 15.0);
-
-        assert_eq!(speeds[50].display_text, "20");
-        assert_eq!(speeds[50].speed_value, 20.0);
-
-        assert_eq!(speeds[51].display_text, "25");
-        assert_eq!(speeds[51].speed_value, 25.0);
-
-        assert_eq!(speeds[52].display_text, "30");
-        assert_eq!(speeds[52].speed_value, 30.0);
+        // Check third filter
+        assert_eq!(filters[2].factor, 1024);
+        assert_eq!(filters[2].fstop_reduction, 10.0);
+        assert_eq!(filters[2].display_name, "ND1000");
+        assert_eq!(filters[2].selected, false);
+        assert_eq!(filters[2].id, 3);
     }
 }
