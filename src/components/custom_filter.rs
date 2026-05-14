5dcfb61da3e532ddf7abe7837b5d292f03ce397f
Stefan Rabmund <stefan.rabmund@mailbox.de>
2026-05-14 18:22:21 +0200

    filter editing

--- src/components/custom_filter.rs
+++ src/components/custom_filter.rs
@@ -31,6 +31,14 @@ pub fn CustomFilter() -> Element {
         Filter::store_filter_list(&sig);
     });
 
+    let edit_callback = use_callback(move |updated: Filter| {
+        let mut sig = filters.write();
+        if let Some(f) = sig.iter_mut().find(|f| f.id == updated.id) {
+            *f = updated;
+        }
+        Filter::store_filter_list(&sig);
+    });
+
     rsx!(
         article {
             h3 { "{custom_filter}" }
@@ -38,7 +46,8 @@ pub fn CustomFilter() -> Element {
                 FilterComponent {
                     key: "{f.id}",
                     filter: f.clone(),
-                    on_remove: remove_callback
+                    on_remove: remove_callback,
+                    on_edit: edit_callback
                 }
             }
             button { onclick: add_filter_handler, "{add_filter}" }
@@ -48,45 +57,78 @@ pub fn CustomFilter() -> Element {
 }
 
 #[component]
-fn FilterComponent(filter: Filter, on_remove: Callback<usize>) -> Element {
+fn FilterComponent(
+    filter: Filter,
+    on_remove: Callback<usize>,
+    on_edit: Callback<Filter>,
+) -> Element {
     let display_name = t!("filter.display_name");
     let fstop_reduction = t!("filter.fstop_reduction");
     let factor = t!("filter.factor");
     let delete = t!("filter.delete");
+
+    let filter_id = filter.id;
+    let filter_display = filter.display_name.clone();
+    let filter_fstop = filter.fstop_reduction;
+    let filter_factor = filter.factor;
+
+    let filter_for_name = filter.clone();
+    let filter_for_fstop = filter.clone();
+    let filter_for_factor = filter.clone();
+
     rsx!(
         article {
             div { "class": "grid",
                 label { "for": "display_name",
                     "{display_name}",
                     input {
-                        value: &*filter.display_name,
                         "type": "text",
                         id: "display_name",
                         name: "display_name",
-                        "required"
+                        "required": true,
+                        value: filter_display.as_str(),
+                        oninput: move |event| {
+                            let mut f = filter_for_name.clone();
+                            f.display_name = event.value();
+                            on_edit.call(f);
+                        }
                     }
                 }
                 label { "for": "fstop_reduction",
                     "{fstop_reduction}",
                     input {
-                        value: "{filter.fstop_reduction}",
                         "type": "number",
                         id: "fstop_reduction",
-                        name: "fstop_reduction"
+                        name: "fstop_reduction",
+                        value: "{filter_fstop}",
+                        oninput: move |event| {
+                            if let Ok(val) = event.value().parse::<f64>() {
+                                let mut f = filter_for_fstop.clone();
+                                f.fstop_reduction = val;
+                                on_edit.call(f);
+                            }
+                        }
                     }
                 }
                 label { "for": "factor",
                     "{factor}",
                     input {
-                        value: "{filter.factor}",
                         "type": "number",
                         id: "factor",
-                        name: "factor"
+                        name: "factor",
+                        value: "{filter_factor}",
+                        oninput: move |event| {
+                            if let Ok(val) = event.value().parse::<u64>() {
+                                let mut f = filter_for_factor.clone();
+                                f.factor = val;
+                                on_edit.call(f);
+                            }
+                        }
                     }
                 }
             }
             div { "align": "right",
-                a { "href": "#", role: "button", onclick: move |e| { e.prevent_default(); on_remove.call(filter.id); }, "{delete}" }
+                a { "href": "#", role: "button", onclick: move |e| { e.prevent_default(); on_remove.call(filter_id); }, "{delete}" }
             }
         }
     )
